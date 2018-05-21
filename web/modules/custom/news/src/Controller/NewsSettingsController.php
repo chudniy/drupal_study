c4524113521b83434977dd4f6654e4eebe9b9264
jack <evgeniychudniy@gmail.com>
2018-05-21 16:59:15 +0300

    Added settings page for news module

---
+++ web/modules/custom/news/src/Controller/NewsSettingsController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Drupal\news\Controller;
+
+/**
+ * Class NewsSettingsController.
+ *
+ * @package Drupal\news\Controller
+ */
+class NewsSettingsController {
+
+    /**
+     * Returns settings page.
+     */
+    public function getSettingsPage() {
+        $service = \Drupal::service('news.sources');
+        return [
+            '#markup' => '<pre>' . json_encode($service->get(), JSON_PRETTY_PRINT) . '</pre>',
+            '#title' => 'News'
+        ];
+    }
+
+}
